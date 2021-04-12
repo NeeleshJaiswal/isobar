8572f02ff59b0675fe465df42cc512de5cd2fded
Neelesh Jaiswal <neelesh.jaiswal@yash.com>
2021-04-12 09:42:37 +0530

    fix: added padding for book cards

--- src/tests/components/App.spec.js
+++ src/tests/components/App.spec.js
@@ -1,8 +1,10 @@
 import { shallow } from "enzyme";
 import App from "../../App";
 
-it('should render app component', () => {
-    const wrapper = shallow(<App/>);
+describe('should have App component', () => {
+    it('should render app component', () => {
+        const wrapper = shallow(<App/>);
 
-    expect(wrapper.type()).toBe('div');
-});
+        expect(wrapper.type()).toBe('div');
+    });
+})
